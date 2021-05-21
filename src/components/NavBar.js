f404d48f0bce89b318467b233d522a079942b1cc
Blake Julander <13lakejulander@gmail.com>
2021-05-21 14:35:32 -0700

    commit tree 9be2bad5111ef7164d36332b8e5d7067890ff67c

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <NavLink exact to="/" style={link}>Home</NavLink>
+      <NavLink exact to="/movies" style={link}>Movies</NavLink>
+      <NavLink exact to="/directors" style={link}>Directors</NavLink>
+      <NavLink exact to="/actors" style={link}>Actors</NavLink>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import App from './containers/App';
 render (
   <App />,
   document.getElementById('root')
-);
+)
